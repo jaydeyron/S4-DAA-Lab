5dfb7f4d411404cba81e8e9f0c02be647f310f57
Jay <94859351+jaydeyron@users.noreply.github.com>
2023-06-07 01:37:34 +0530

    Update tsp.cpp

--- Experiment 3/Travelling Salesman Problem/tsp.cpp
+++ Experiment 3/Travelling Salesman Problem/tsp.cpp
@@ -4,20 +4,23 @@
 #include<utility>
 #include<algorithm>
 #include<iterator>
-
+#define intmax __INT_MAX__
 using namespace std;
 
 //global variables
 int n;
-vector<vector<int> >mat;
+vector<int> path;
+vector<vector<int> >mat;  vector<vector<int> > memo;
 
 //functions
-void insertMat(){
+void insertMat(vector<vector<int> > memo){
     int a;
     for(int i=0;i<n;i++){
-        vector<int> vec1;   mat.push_back(vec1);
+        vector<int> vec0;  memo.push_back(vec0);  mat.push_back(vec0);
+        path.push_back(0);
         for(int j=0;j<n;j++){
-            cin>>a; mat[i].push_back(a);
+            cin>>a; memo[i].push_back(-1);
+            mat[i].push_back(a);
         }
     }
 }
@@ -32,15 +35,46 @@ void printMat(){
         cout<<s;    s++;
         for(int j=0;j<n;j++){
             cout<<' '<<mat[i][j];
-        }
-        printf("\n\n");
+        }   printf("\n\n");
     }
 }
 
+int tsp(int i, vector<int> temp){
+    int res=intmax;
+    if(temp.size()==0 && i!=0){
+        return mat[i][0];
+    }
+    for(int j=0;j<temp.size();j++){
+        //if(i==j){continue;}
+        int tp=temp[j];
+        temp.erase(temp.begin()+j);
+        //res=min(res, mat[i][j]+tsp(i,vector<int> (temp)));
+        int mn=mat[i][tp]+tsp(tp,vector<int>(temp));
+        if(mn<res){
+            res=mn; path[i]=tp;
+        }
+        //cout<<res<<' ';
+        temp.insert(temp.begin()+j,tp);
+    }
+    return res;
+}
+
+void printPath(){
+    cout<<"\nPath:\t1";
+    for(int i=0;i<n;i++){
+        cout<<"-->"<<path[i]+1;
+    }
+}
 //main
 int main(){
     cout<<"Enter the number of cities:\t"; cin>>n;
-    insertMat();
+    vector<int> temp;
+    for(int i=1;i<n;i++){temp.push_back(i);}
+    insertMat(memo);
     printMat();
+    //cout<<(temp.size())<<'\n';
+    int mincost=tsp(0,temp);
+    cout<<"Minimum cost:\t"<<mincost;
+    //printPath();
     return 0;
 }
